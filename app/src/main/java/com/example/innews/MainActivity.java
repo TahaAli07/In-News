0d57caeaee9e53f5513a9db0d69bf720fc7420b6
Taha Ali <taslash74@gmail.com>
2019-02-01 17:40:49 +0530

    Added retrofit to fetch data from API

--- app/src/main/java/com/example/innews/MainActivity.java
+++ app/src/main/java/com/example/innews/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.innews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+
+//API KEY - 8fca5a6d12fd4bf98757828ba3833f64
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getData();
+    }
+
+    private void getData(){
+        Call<PostList> postList = NewsAPI.getNewsService().getPostList();
+        postList.enqueue(new Callback<PostList>() {
+            @Override
+            public void onResponse(Call<PostList> call, Response<PostList> response) {
+                PostList list = response.body();
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<PostList> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Failure", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/innews/NewsAPI.java
@@ -0,0 +1,37 @@
+package com.example.innews;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+public class NewsAPI {
+
+    private static final String KEY = "8fca5a6d12fd4bf98757828ba3833f64";
+    private static  final String BASE_URL = "https://newsapi.org/";
+    //https://newsapi.org/v2/everything?q=ArtificialIntelligence&from=2019-01-01&sortBy=publishedAt
+
+    public static NewsService newsService = null;
+
+
+    //for Singleton pattern
+    public static NewsService getNewsService(){
+        if(newsService==null){
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            newsService = retrofit.create(NewsService.class);
+        }
+        return newsService;
+    }
+
+    //Create a interface
+    //Provide methods for interface
+    public interface NewsService{
+
+        @GET("v2/everything?q=ArtificialIntelligence&from=2019-01-01&sortBy=publishedAt"+"&apiKey="+KEY)
+        Call<PostList> getPostList();
+    }
+}
